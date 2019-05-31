6a5510079f7ee678a66b1dd0a47d2b8b70ee3026
martinblancotl <martinb98blanco@hotmail.com>
2019-05-31 15:27:54 -0300

    prueba

--- src/exceptions/DAOException.java
+++ src/exceptions/DAOException.java
@@ -1,13 +1,9 @@
 package exceptions;
 
+@SuppressWarnings("serial")
 public class DAOException extends Exception {
  
-    /**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
-	public DAOException(String message, Throwable cause) {
+    public DAOException(String message, Throwable cause) {
     	super(message, cause);
     }
 }
